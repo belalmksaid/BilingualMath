f093d8dd41b29046f30502a89474541c9f96151f
Belal Said <belalmksaid@gmail.com>
2017-12-11 22:54:58 -0500

    Add testing routines

--- test.cpp
+++ test.cpp
@@ -4,32 +4,93 @@
 #include <chrono>
 #include "math/util.h"
 #include "math/math.h"
-#include "brt/core/ray.h"
 
-#define K 4
-#define S 3
+#define LOOP 1000000
+#define A 4
 
 using namespace std;
 using namespace math;
 
 int main()
 {
+    cout << "Bilingual Math Library" << endl;
+    cout << "Initializing..." << endl;
     math::init();
-    vector<S> v;
-    v(0) = 3;
-    v(1) = 4;
-    v(2) = 5;
-
-    matrix<K,K> m = matrix<K, K>::random();
-    
-    //math::matrix<K> k;
-    auto start = chrono::high_resolution_clock::now();
-    for (int i = 0; i < 1000000; i++)
+#ifdef DOUBLE_PREC
+    cout << "Double mode in use..." << endl;
+#else
+    cout << "Float mode in use.." << endl;
+#endif
+    cout << "Number of iterations: \t\t" << LOOP << endl;
     {
-        m.inverse();        
+        auto start = chrono::high_resolution_clock::now();
+        for(int i = 0; i < LOOP; i++) {
+        }
+        auto elapsed = chrono::high_resolution_clock::now() - start;
+        long long microseconds = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
+        cout << "Time to run through "  << LOOP << " loop:\t\t" << microseconds << " us"<< endl;
     }
-    auto elapsed = chrono::high_resolution_clock::now() - start;
-    long long microseconds = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
-    cout << microseconds << endl;
+    {
+        auto start = chrono::high_resolution_clock::now();
+        prec holder;
+        for(int i = 0; i < LOOP; i++) {
+            math::random();
+        }
+        auto elapsed = chrono::high_resolution_clock::now() - start;
+        long long microseconds = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
+        cout << "Generate random numbers:\t\t" << microseconds << " us"<< endl;
+    }
+    {
+        matrix<A, A> m;
+        auto start = chrono::high_resolution_clock::now();
+        for(int i = 0; i < LOOP; i++) {
+            m = m;
+        }
+        auto elapsed = chrono::high_resolution_clock::now() - start;
+        long long microseconds = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
+        cout << "Assign matrices:\t\t" << microseconds << " us"<< endl;
+    }
+    {
+        matrix<A, A> m;
+        auto start = chrono::high_resolution_clock::now();
+        for(int i = 0; i < LOOP; i++) {
+            m = matrix<A, A>::identity();
+        }
+        auto elapsed = chrono::high_resolution_clock::now() - start;
+        long long microseconds = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
+        cout << "Generate identity matrices:\t\t" << microseconds << " us"<< endl;
+    }
+    {
+        matrix<A, A> m;
+        auto start = chrono::high_resolution_clock::now();
+        for(int i = 0; i < LOOP; i++) {
+            m = matrix<A, A>::random();
+        }
+        auto elapsed = chrono::high_resolution_clock::now() - start;
+        long long microseconds = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
+        cout << "Generate random matrices:\t\t" << microseconds << " us"<< endl;
+    }
+    {
+        matrix<A, A> m = matrix<A, A>::random();
+        auto start = chrono::high_resolution_clock::now();
+        for(int i = 0; i < LOOP; i++) {
+            m = matrix<A, A>::random();
+            m.transpose();
+        }
+        auto elapsed = chrono::high_resolution_clock::now() - start;
+        long long microseconds = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
+        cout << "Transpose matrices:\t\t" << microseconds << " us"<< endl;
+    }
+    {
+        matrix<A, A> m = matrix<A, A>::random();
+        auto start = chrono::high_resolution_clock::now();
+        for(int i = 0; i < LOOP; i++) {
+            m = matrix<A, A>::random();
+            m.inverse();
+        }
+        auto elapsed = chrono::high_resolution_clock::now() - start;
+        long long microseconds = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
+        cout << "Inverse matrices:\t\t" << microseconds << " us"<< endl;
+    }
     return 0;
 }
